41e296a277af246b079de33161bbc76d747c6f40
Chixphax <122481522+Chixphax@users.noreply.github.com>
2023-03-01 20:17:49 +0100

    Add files via upload

---
+++ entornos/src/entornos/Entornos.java
@@ -0,0 +1,18 @@
+package entornos;
+
+/**
+ *
+ * @author damt121
+ */
+public class Entornos {
+
+   
+    public static void main(String[] args) {
+        
+        double SextaPotenciaDos = Math.pow(2,6);
+      
+        
+        System.out.println(SextaPotenciaDos);
+    }
+    
+}
